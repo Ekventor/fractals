1995560ffb252aba9b1aa2958a859ebbc25481e4
Ekventor <78481240+Ekventor@users.noreply.github.com>
2021-05-14 15:18:29 +0600

    Update fractals.py

--- fractals.py
+++ fractals.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 import traceback
 from numba import jit
 
-TOKEN = '62daa9aaf9641613bd1c9629190d580b5ee4a94ae7ed70a7a1afd86f60ad9f61b45cfb01a138cf89e5e29'
+
+TOKEN = "access_token"
 
 vk = vk_api.VkApi(token=TOKEN)
 vk._auth_token()
@@ -16,8 +17,8 @@ upload = VkUpload(vk)
 ids = []
 
 
-def mandelbrot(formula, author_id, peer_id, is_club=False, filename='fractal.png'):
-    calculate_z = jit(nopython=True, parallel=True)(eval(f'lambda z, c, p, q, k: {formula}'))
+def mandelbrot(formula, author_id, peer_id, is_club=False, filename="fractal.png"):
+    calculate_z = jit(nopython=True, parallel=True)(eval(f"lambda z, c, p, q, k: {formula}"))
 
     def get_fractal(pmin, pmax, ppoints, qmin, qmax, qpoints, max_iterations, infinity_border):
         image = np.zeros((ppoints, qpoints))
@@ -37,26 +38,27 @@ def mandelbrot(formula, author_id, peer_id, is_club=False, filename='fractal.png
 
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(image, cmap='flag', interpolation='none')
+    plt.imshow(image, cmap="flag", interpolation="none")
 
     fig = plt.gcf()
     fig.set_size_inches(20, 20)
 
-    plt.axis('off')
-    plt.savefig(filename, format='png', bbox_inches='tight')
+    plt.axis("off")
+    plt.savefig(filename, format="png", bbox_inches="tight")
 
     photo_list = upload.photo_messages(filename)
-    attachment = ','.join('photo{owner_id}_{id}'.format(**item) for item in photo_list)
+    attachment = ",".join("photo{owner_id}_{id}".format(**item) for item in photo_list)
 
-    vk.method('messages.send', {'peer_id': peer_id,
-                                'message': f'''[{'id' if not is_club else 'public'}{author_id}|Автор]\nФормула: {formula}''',
-                                'attachment': attachment, 'random_id': 0})
+    vk.method("messages.send", {"peer_id": peer_id,
+                                "message": f'[{"id" if not is_club else "public"}{author_id}|Автор]\nФормула: {formula}',
+                                "attachment": attachment, 
+                                "random_id": 0})
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def main():
     try:
         try:
@@ -64,11 +66,11 @@ def main():
         except json.decoder.JSONDecodeError:
             return "ok"
 
-        if data['type'] == 'confirmation':
-            return 'ebeb2f7f'
+        if data["type"] == "confirmation":
+            return "callback_string"
 
-        elif data['type'] == 'message_new':
-            user_id = data['object']['message']['from_id']
+        elif data["type"] == "message_new":
+            user_id = data["object"]["message"]["from_id"]
 
             if user_id < 0:
                 is_club = True
@@ -76,24 +78,22 @@ def main():
             else:
                 is_club = False
 
-            peer_id = data['object']['message']['peer_id']
-            message = data['object']['message']['text']
-            message_id = data['object']['message']['conversation_message_id']
+            peer_id = data["object"]["message"]["peer_id"]
+            message = data["object"]["message"]["text"]
+            message_id = data["object"]["message"]["conversation_message_id"]
 
-            if '/фрактал' in message:
-                if f'{peer_id}: {message_id}' not in ids:
-                    if 'club' in message:
-                        formula = message.split(' ', 2)[2]
+            if "/фрактал" in message:
+                if f"{peer_id}: {message_id}" not in ids:
+                    if "club" in message:
+                        formula = message.split(" ", 2)[2]
                     else:
-                        formula = message.split(' ', 1)[1]
+                        formula = message.split(" ", 1)[1]
 
-                    ids.append(f'{peer_id}: {message_id}')
+                    ids.append(f"{peer_id}: {message_id}")
                     mandelbrot(formula, user_id, peer_id, is_club)
 
     except:
-        vk.method('messages.send', {'user_id': 386746383,
-                                    'message': traceback.format_exc(),
-                                    'random_id': 0})
+        pass
 
     finally:
-        return 'ok'
+        return "ok"
